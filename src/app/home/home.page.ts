766db12432d0118b8dd7d8bd41c7e4f0dd263a8a
amdress <amdresscavacal@gmail.com>
2024-08-21 12:23:59 -0300

    agregando alerts para el chekbox

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 interface Task {
   name: String;
@@ -12,22 +13,49 @@ interface Task {
 })
 export class HomePage {
 
+  isVisible : boolean = false;
   newTask : string = '';
   tasks : Task[] = [];
 
-  constructor() {}
+  constructor(
+    private alertCtrl : AlertController
+  ) {}
 
   addTask() {
     if(this.newTask.trim().length > 0){
-      this.tasks.push({name: this.newTask, completed: false});
+      this.isVisible = true;
 
+      this.tasks.push({name: this.newTask, completed: false});
+      
       this.newTask = '';
+      this.presentAlert('registro exitoso', 'tarea guardada correctamente');
     }
 
   }
+async onTaskStatusChange(task : Task){
+  const subHeader = task.completed ? 'Tarea Completada con Exito' : 'Tarea Incompleta';
+  const message = task.completed 
+  ? 'La tarea se ha completada correctamente'
+  : 'La tarea se ha marcado como incompleta';
+
+  await this.presentAlert( subHeader, message)
+
+  
+}
 
   deleteTask(task : Task){
     this.tasks = this.tasks.filter(t => t !==task);
+
+    this.presentAlert('Borrando', 'La Tarea se ha borrado por completo')
   }
 
+  async presentAlert( sub : string , msg : string){
+    const alert = await this.alertCtrl.create({
+      header: 'Alerta',
+      subHeader: sub,
+      message: msg,
+      buttons: [' OK']
+    })
+    await alert.present();
+  }
 }
